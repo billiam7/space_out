4a692917cbff9da73df7a65b13f893a4c43efdfc
Mario Rosenthal <mario.f.rosenthal@gmail.com>
2017-02-13 15:52:30 -0500

    added floating element in css and filter in background to darken backround image

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,22 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+$(document).ready(function() {
+    //if no method and options specified
+    //start the floating animation with defaults settings
+    // $('object').jqFloat();
+
+    //if only options specified
+    //start the floating animation with specified settings
+    // $('object').jqFloat({
+    //     width: 300,
+    //     height: 300,
+    //     speed: 100
+    // });
+
+    //stop the floating animation
+    $('h1').jFloat('stop');
+
+    // //start the floating animation
+    // $('object').jFloat('play');
+});
