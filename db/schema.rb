cef12d9540f70b1ebcf7149b6520472f26b11594
Mario Rosenthal <mario.f.rosenthal@gmail.com>
2017-02-13 17:02:46 -0500

    minor changes in html

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170212183256) do
+ActiveRecord::Schema.define(version: 20170213211519) do
+
+  create_table "todo_items", force: :cascade do |t|
+    t.string   "description"
+    t.datetime "due_date"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
